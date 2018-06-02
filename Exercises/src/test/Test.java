5e9b5cdbda119e5b3baf7484ff6e53a844e5ccb9
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-06-02 17:23:24 -0600

    Initial commit and TestAlgs4

---
+++ Exercises/src/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author ethanbrown
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+
+	}
+
+}
